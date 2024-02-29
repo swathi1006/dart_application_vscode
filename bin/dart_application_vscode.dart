87efc4d658915d402426796d02da390a04899b2c
swathi1006 <swathisahadevan996@gmail.com>
2024-02-29 15:28:05 +0530

    first commit

---
+++ bin/dart_application_vscode.dart
@@ -0,0 +1,5 @@
+import 'package:dart_application_vscode/dart_application_vscode.dart' as dart_application_vscode;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_application_vscode.calculate()}!');
+}
